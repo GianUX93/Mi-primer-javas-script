01d8b1d942a32be9c5ee2e10a4951bd7dfd16cf5
Gian Carlos Chavez Sanchez <g.carloschavez@gmail.com>
2026-06-20 21:44:40 -0500

    condicionales js

--- script.js
+++ script.js
@@ -1,45 +1,11 @@
-// ==========================
-// DATOS PERSONALES
-// ==========================
-const nombre = 'Gian Carlos Chavez Sanchez';
-const edad = 32;
-const distrito = 'Lurín';
-const oficio = 'product designer';
+let edad = prompt("¿Cuál es tu edad?");
 
-// Mostrar valor y tipo
-console.log(nombre, typeof nombre);
-console.log(edad, typeof edad);
-console.log(distrito, typeof distrito);
-console.log(oficio, typeof oficio);
+edad = Number(edad);
 
-// ==========================
-// MENSAJES
-// ==========================
-
-// Forma vieja (concatenación)
-const msg1 = 'Hola soy ' + nombre + ', tengo ' + edad + ' años y vivo en ' + distrito + '.';
-
-// Forma moderna (template literals)
-const msg2 = `Hola soy ${nombre}, tengo ${edad} años y vivo en ${distrito}.`;
-
-console.log(msg1);
-console.log(msg2);
-
-// ==========================
-// JUEGO
-// ==========================
-
-const numeroSecreto = 5;
-let intento = prompt("Adivina el número del 1 al 10");
-
-// Operadores matemáticos
-console.log("Suma:", 2 + 2);
-console.log("Resta:", 10 - 3);
-console.log("Multiplicación:", 4 * 5);
-
-// Resultado
-if (Number(intento) === numeroSecreto) {
-  alert("🎉 Correcto");
+if (edad < 13) {
+    alert("👦Eres menor de edad, aún no tienes la edad suficiente para votar😔.");
+} else if (edad >= 13 && edad < 18) {
+    alert("👦Eres un adolescente, Tienes DNI pero no puedes votar🥹.");
 } else {
-  alert("❌ Incorrecto");
+    alert("👨Eres mayor de edad, si puedes votar🎉.");
 }

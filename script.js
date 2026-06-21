abac3c84982f312a7b24214de139b9b86f23b901
Gian Carlos Chavez Sanchez <g.carloschavez@gmail.com>
2026-06-20 22:02:41 -0500

    condicionales js - clase 10

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let edad = prompt("¿Cuál es tu edad?");
 edad = Number(edad);
 
 if (edad < 13) {
-    alert("👦Eres menor de edad, aún no tienes la edad suficiente para votar😔.");
+    alert("👦Eres menor de edad, aún no tienes la edad suficiente para votar😔");
 } else if (edad >= 13 && edad < 18) {
-    alert("👦Eres un adolescente, Tienes DNI pero no puedes votar🥹.");
+    alert("👦Eres un adolescente, Tienes DNI pero no puedes votar🥹");
 } else {
-    alert("👨Eres mayor de edad, si puedes votar en la ONPE🎉.");
+    alert("👨Eres mayor de edad, si puedes votar en la ONPE🎉");
 }
